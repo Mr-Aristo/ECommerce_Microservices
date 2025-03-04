cc65d66bb6b49d827cdeddca63ba85ad33334667
Emre <80692170+Mr-Aristo@users.noreply.github.com>
2025-03-04 22:04:46 +0300

    Localdev (#8)
    * package error solved
    
    * update serviS
    
    ---------
    
    Co-authored-by: Emre Geçer <unknown_ddos@hotmail.com>

---
+++ Src/Services/Order/Order.Application/Exception/OrderNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlock.Exceptions;
+
+namespace Order.Application.Extentions;
+
+public class OrderNotFoundException : NotFoundExceptions
+{
+    public OrderNotFoundException(Guid id) : base("Order", id)
+    {
+
+    }
+
+}
---
+++ Src/Services/Order/Order.Application/Services/RandomCodeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Order.Application.Services;
+
+public static class RandomCodeGenerator
+{
+    /// <summary>
+    /// Generates Trace Code
+    /// </summary>
+    /// <param name="lenght"></param>
+    /// <returns></returns>
+    public static string GenerateCode(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new();
+        StringBuilder result = new(length);
+
+        for (int i = 0; i < length; i++)
+            result.Append(chars[random.Next(chars.Length)]);
+
+        return result.ToString();
+
+    }
+
+}
