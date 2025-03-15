18e631e1c7522455c49a62a1356df03e8f00a6e8
Emre <80692170+Mr-Aristo@users.noreply.github.com>
2025-03-15 12:50:03 +0300

    Localdev (#12)
    * package error solved
    
    update serviS
    
    test
    
    New logics added
    
    * package error solved
    
    update serviS
    
    test
    
    New logics added
    
    * Command logic added.
    
    * CQRS logic completed
    
    * integration event added
    
    ---------
    
    Co-authored-by: Emre Geçer <unknown_ddos@hotmail.com>

---
+++ Src/Services/Order/Order.Application/OrdersCQRS/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,54 @@
+﻿
+using BuildingBlockMessaging.Events;
+using MassTransit;
+using Order.Application.OrdersCQRS.Commands.CreateOrder;
+using Order.Domain.ValueObjects;
+
+namespace Order.Application.OrdersCQRS.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger) : IConsumer<BasketCheckoutEvent>
+{
+    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private CreateOrderCommand MapToOrderCommand(BasketCheckoutEvent message)
+    {
+        // Create full order with incoming event data
+        var addressDto = new AddressDto(
+            message.FirstName,
+            message.LastName,
+            message.EmailAddress,
+            message.AddressLine,
+            message.Country,
+            message.State,
+            message.ZipCode);
+
+        var paymentDto = new PaymentDto(
+            message.CardName,
+            message.CardNumber,
+            message.Expiration,
+            message.CVV,
+            message.PaymentMethod);
+
+        var orderId = Guid.NewGuid();
+
+
+        var orderDto = new OrderDto(
+            Id: orderId,
+            CustomerId: message.CustomerId,
+            OrderName: message.UserName,
+            ShippingAddress: addressDto,
+            BillingAddress: addressDto,
+            Payment: paymentDto,
+            Status: Order.Domain.Enums.OrderStatus.Pending,
+            OrderItems:
+            [
+                new OrderItemDto(orderId, new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"), 2, 500),
+                new OrderItemDto(orderId, new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"), 1, 400)
+            ]);
+
+        return new CreateOrderCommand(orderDto);
+    }
+}
