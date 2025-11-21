3eedd152b5812dde532ff612d781e7bcf21adf2f
Emre <80692170+Mr-Aristo@users.noreply.github.com>
2025-11-21 14:49:09 +0300

    ELK stack added. (#26)
    Co-authored-by: Emre <Emre@DESKTOP-8FMJR1I>

--- Src/Services/CatalogAPI/Program.cs
+++ Src/Services/CatalogAPI/Program.cs
@@ -1,46 +1,70 @@
-
-var builder = WebApplication.CreateBuilder(args);
-
-//Add services  to the container.
-
 //The reason of the typeof(Program).Assembly to provide to run commands and queriesd only in this progran.cs
 var assembly = typeof(Program).Assembly;
 
-builder.Services.AddMediatR(config =>
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("ServiceName", "CatalogAPI")
+    .WriteTo.Console(new RenderedCompactJsonFormatter())
+    .CreateLogger();
+
+try
 {
-    config.RegisterServicesFromAssembly(assembly);
-    //Registration of Pipeline behavior
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));//<,> that means generic
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
 
-builder.Services.AddValidatorsFromAssembly(assembly);
-builder.Services.AddCarter();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMarten(options =>
+    //Add services  to the container.
+
+    builder.Services.AddMediatR(config =>
+    {
+        config.RegisterServicesFromAssembly(assembly);
+        //Registration of Pipeline behavior
+        config.AddOpenBehavior(typeof(ValidationBehavior<,>));//<,> that means generic
+        config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    });
+
+    builder.Services.AddValidatorsFromAssembly(assembly);
+    builder.Services.AddCarter();
+
+    builder.Services.AddMarten(options =>
+    {
+        options.Connection(builder.Configuration.GetConnectionString("PostgreDataBase")!);
+    }).UseLightweightSessions();
+
+    //Data seeding
+    if (builder.Environment.IsDevelopment())
+        builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+    builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+    //HealthCheck
+    builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("PostgreDataBase")!);
+
+    var app = builder.Build();
+    // Configure the HTTP request pipline.
+
+    //Serilog take requests
+    app.UseSerilogRequestLogging();
+
+    app.MapCarter();
+
+    app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+    {   //HealthCheck with psql
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+    app.UseExceptionHandler(options => { });
+
+    app.Run();
+
+}
+catch (Exception ex)
 {
-    options.Connection(builder.Configuration.GetConnectionString("PostgreDataBase")!);
-}).UseLightweightSessions();
 
-//Data seeding
-if (builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-//HealthCheck
-builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("PostgreDataBase")!);
-
-var app = builder.Build();
-// Configure the HTTP request pipline.
-
-app.MapCarter();
-
-app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
-{   //HealthCheck with psql
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-
-app.UseExceptionHandler(options => { });
-
-app.Run();
+    Log.Fatal(ex,"CatalogService terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
